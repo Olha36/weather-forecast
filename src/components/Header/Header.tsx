0e5a1d863083618410d0e43765122e9fbd576fe2
olha36 <olhakucheruk36@gmail.com>
2025-11-30 10:20:03 +0200

    fix favourites in header logic

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,96 +1,3 @@
-// 'use client';
-// import Avatar from '@mui/material/Avatar';
-// import { Button } from '../ui/Button';
-// import { Box } from '@mui/material';
-// import Image from 'next/image';
-// import Link from 'next/link';
-// import logo from '../../assets/logo.png';
-// import { useEffect, useState } from 'react';
-// import { useRouter } from 'next/navigation';
-// import favouriteIcon from '../../assets/favourite.png';
-
-// export default function Header() {
-//   const [isLoggedIn, setIsLoggedIn] = useState(false);
-//   const router = useRouter();
-
-//   useEffect(() => {
-//     const cookies = document.cookie.split(';').map((c) => c.trim());
-//     const jwtCookie = cookies.find((c) => c.startsWith('jwt='));
-//     if (jwtCookie) {
-//       setIsLoggedIn(true);
-//     }
-//   }, []);
-
-//   const handleAuthClick = () => {
-//     if (isLoggedIn) {
-//       document.cookie = 'jwt=; path=/; max-age=0';
-//       setIsLoggedIn(false);
-//       router.push('/');
-//     } else {
-//       router.push('/auth/signin');
-//     }
-//   };
-
-//   return (
-//     <div
-//       className="header-container"
-//       style={{
-//         display: 'flex',
-//         alignItems: 'center',
-//         justifyContent: 'space-between',
-//         padding: '33px',
-//       }}
-//     >
-//       <Link href="/">
-//         <Image src={logo} alt="logo" style={{ cursor: 'pointer' }} />
-//       </Link>
-//       <nav>
-//         <Link href="/about">Who we are</Link>
-//         <Link href="/contact" style={{ margin: '0 42px' }}>
-//           Contacts
-//         </Link>
-//         <Link href="/menu">Menu</Link>
-//       </nav>
-//       <Box style={{ display: 'flex', alignItems: 'center' }}>
-//         {isLoggedIn && (
-//           <Link href={'/favourite'}>
-//             <Image
-//               src={favouriteIcon}
-//               width={30}
-//               height={30}
-//               alt="favourite icon"
-//             ></Image>
-//           </Link>
-//         )}
-
-//         <Button
-//           size="default"
-//           variant="default"
-//           style={{
-//             backgroundColor: '#FFB36C',
-//             color: '#000000',
-//             margin: '0 27px',
-//           }}
-//           onClick={handleAuthClick}
-//         >
-//           {isLoggedIn ? 'Log Out' : 'Sign In'}
-//         </Button>
-
-//         <Avatar
-//           alt="User Avatar"
-//           src={
-//             isLoggedIn
-//               ? '/static/images/avatar/2.jpg'
-//               : '/static/images/avatar/default.jpg'
-//           }
-//           style={{ width: '50px', height: '50px' }}
-//         />
-//       </Box>
-//     </div>
-//   );
-// }
-
-
 'use client';
 
 import Avatar from '@mui/material/Avatar';
@@ -128,6 +35,13 @@ export default function Header() {
       router.push('/auth/signin');
     }
   };
+  const handleAddFavouritesClick = () => {
+    if(isLoggedIn) {
+      router.push('/favourite');
+    } else {
+      router.push('/auth/signin');
+    }
+  }
 
   return (
     <div
@@ -150,14 +64,24 @@ export default function Header() {
         <Link href="/menu">Menu</Link>
       </nav>
       <Box style={{ display: 'flex', alignItems: 'center' }}>
-        <Link href={'/favourite'}>
+        {isLoggedIn ? (
+          <Link href={'/favourite'}>
+            <Image
+              src={favouriteIcon}
+              width={30}
+              height={30}
+              alt="favourite icon"
+            />
+          </Link>
+        ) : (
           <Image
             src={favouriteIcon}
             width={30}
             height={30}
             alt="favourite icon"
+            onClick={handleAddFavouritesClick}
           />
-        </Link>
+        )}
 
         <Button
           size="default"
