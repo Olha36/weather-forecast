fd45ab706ff55069dae649c7a8c9e596130143e3
olha36 <olhakucheruk36@gmail.com>
2025-12-07 17:59:36 +0200

    fix key variable for deployment

--- src/api/newsApi/newsApi.ts
+++ src/api/newsApi/newsApi.ts
@@ -1,4 +1,5 @@
-const NEWS_API_KEY = '1ae02c4bf69a4a1882b8019165404a8f';
+const NEWS_API_KEY =
+  process.env.NEWS_API_KEY || '1ae02c4bf69a4a1882b8019165404a8f';
 
 export const getNews = async () => {
   const result = await fetch(
